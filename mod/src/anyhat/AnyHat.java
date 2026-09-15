12a064a88fceca142e9ab00b75e2f6ca6393eab7
AnCarsenat <antoine.carsenat@gmail.com>
2026-09-15 17:59:32 +0200

    Release on version tags and package as mod

---
+++ mod/src/anyhat/AnyHat.java
@@ -0,0 +1,6 @@
+package anyhat;
+
+// Empty entrypoint: (Neo)Forge only loads a jar's data/ folder if it declares a @Mod class
+@net.minecraftforge.fml.common.Mod("mr_any_hat")
+public class AnyHat {
+}
---
+++ mod/stubs/net/minecraftforge/fml/common/Mod.java
@@ -0,0 +1,10 @@
+package net.minecraftforge.fml.common;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+// Compile-time stub only, not shipped in the jar
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Mod {
+    String value();
+}
